06d377fe81df321e8daa2848cdcfbb784d523877
Francis Barton <fbarton@alwaysdata.net>
2020-12-07 03:21:01 +0000

    Day 6 :-)

--- 20201206.R
+++ 20201206.R
@@ -1,2 +1,43 @@
 # Day 6
 
+# Part 1
+library(purrr)
+library(stringr)
+
+input <- readLines("day6_input")
+
+input[which(input == "")] <- "EOL"
+
+
+input %>%
+  str_c(collapse = "") %>%
+  str_split("EOL") %>%
+  unlist() %>%
+  str_extract_all("[a-z]") %>%
+  map(unique) %>%
+  map(length) %>%
+  unlist() %>%
+  sum()
+
+
+# Part 2
+
+check_in_all <- function(string) {
+  string_members <- str_split(string, ",") %>%
+    unlist()
+
+  string_letters <- str_extract_all(string_members, "[a-z]") %>%
+    unlist() %>%
+    unique()
+
+  map_lgl(string_letters, ~ all(str_detect(string_members, .))) %>%
+    sum()
+}
+
+
+input %>%
+  str_c(collapse = ",") %>%
+  str_split(",EOL,") %>%
+  pluck(1) %>%
+  map_int(check_in_all) %>%
+  sum()
