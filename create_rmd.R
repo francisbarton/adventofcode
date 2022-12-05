6ed73f29407527932b08ecc909b3f8815cbf634e
Francis Barton <francis.barton@nuh.nhs.uk>
2022-12-05 22:39:56 +0000

    days 1-5 (Rmd only)

---
+++ create_rmd.R
@@ -0,0 +1,18 @@
+post_title <- readline("Post title: ")
+post_slug <- tolower(gsub(" ", "_", post_title))
+year <- format(Sys.Date(), "%Y")
+month <- format(Sys.Date(), "%m")
+file_loc <- here::here(paste0(post_slug, ".Rmd"))
+yaml_block <- paste(
+  "---",
+  paste0("title: \"", post_title, "\""),
+  paste0("date: ", Sys.Date()),
+  "output: md_document",
+  "subtitle: ",
+  "tags: ",
+  "- advent_of_code",
+  "- rstats",
+  "---",
+  sep = "\n"
+)
+writeLines(yaml_block, file_loc)
