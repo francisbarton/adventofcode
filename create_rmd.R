ec1dfc00f942c926bb0e77986d35e01d9c7d0dab
Francis Barton <francis.barton@nuh.nhs.uk>
2022-12-06 13:42:44 +0000

    Day 6 complete

--- create_rmd.R
+++ create_rmd.R
@@ -2,10 +2,10 @@ post_title <- readline("Post title: ")
 post_slug <- tolower(gsub(" ", "_", post_title))
 year <- format(Sys.Date(), "%Y")
 month <- format(Sys.Date(), "%m")
-file_loc <- here::here(paste0(post_slug, ".Rmd"))
+file_loc <- here::here(year, paste0(post_slug, ".Rmd"))
 yaml_block <- paste(
   "---",
-  paste0("title: \"", post_title, "\""),
+  paste0("title: '", post_title, "'"),
   paste0("date: ", Sys.Date()),
   "output: md_document",
   "subtitle: ",
