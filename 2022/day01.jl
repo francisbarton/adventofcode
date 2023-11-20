093f24763b34279ab2e35746e91445d6fa02bfa4
Fran Barton <francis.barton@nuh.nhs.uk>
2023-11-20 10:41:11 +0000

    Shorter v2 in Julia for day 1

--- 2022/day01.jl
+++ 2022/day01.jl
@@ -1,25 +1,16 @@
 input_raw = readlines("2022/input/day_01");
 
-function conv_int(x)
-  if x == "" missing
-  else parse(Int64,x)
-  end
+breaks = findall(x -> x == "", input_raw);
+input_vec = map(x -> if x == "" 0 else parse(Int64, x) end, input_raw);
+
+x = Iterators.take(breaks, length(breaks) - 1);
+y = Iterators.drop(breaks, 1);
+
+z = zip(x, y);
+
+function pull_diff(x)
+  sum(input_vec[1:x[2]]) - sum(input_vec[1:x[1]])
 end;
 
-input_vec = map(x -> conv_int(x), input_raw);
-breaks = findall(ismissing, input_vec);
-input_vec = map(x -> if ismissing(x) 0 else x end, input_vec);
-
-function pull_diff(x, vec1, vec2)
-  total1 = sum(vec2[1:vec1[x]])
-  if x > 1
-    total2 = sum(vec2[1:vec1[x-1]])
-  elseif x == 1
-    total2 = 0
-  end
-  total1 - total2
-end;
-
-calories_list = map(x -> pull_diff(x, breaks, input_vec), range(1, length(breaks)));
-maximum(calories_list)
-reduce(max, calories_list)
+calories_list = map(pull_diff, z);
+maximum(collect(calories_list))
